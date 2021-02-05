0ee1dc2dfe28f753f10a489086c7706b368a81b7
3lnumag <3lnumag@gmail.com>
2021-02-04 21:08:03 -0600

    [UPD] manifest

--- lob/models/lob.py
+++ lob/models/lob.py
@@ -4,15 +4,16 @@ from odoo import fields, models
 
 
 class Lob(models.Model):
-    _name = 'lob'
-    _description = 'Línea de negocio'
+   _name = 'lob'
+   _description = 'Línea de negocio'
 
+   name = fields.Char('Nombre', required=True)
+   code = fields.Char('Código', required=True)
+   image = fields.Binary(attachment=True)
 
-    name = fields.Char('Nombre', required=True)
-    code = fields.Char('Código', required=True)
-    image = fields.Binary(attachment=True)
-
-    scale_entrance = fields.Boolean('Báscula de entrada')
-    scale_exit = fields.Boolean('Báscula de salida')
-    entrance_seq_id = fields.Many2one('ir.sequence', 'Secuencia de báscula de entrada')
-    exit_seq_id = fields.Many2one('ir.sequence', 'Secuencia de báscula de sálida')
+   scale_entrance = fields.Boolean('Báscula de entrada')
+   scale_exit = fields.Boolean('Báscula de salida')
+   entrance_seq_id = fields.Many2one('ir.sequence',
+                                     'Secuencia de báscula de entrada')
+   exit_seq_id = fields.Many2one('ir.sequence',
+                                 'Secuencia de báscula de sálida')
--- lob_extend/models/__init__.py
+++ lob_extend/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import scale_exit
-from . import scale_driver
-from . import exit_orderline
+from . import sale_order
+from . import purchase_order
