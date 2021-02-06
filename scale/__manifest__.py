79fa420154d8eb788bd602cad797783c15df45ff
3lnumag <3lnumag@gmail.com>
2021-02-05 22:03:52 -0600

    [UPD] scale_entrance, scale_reporting

--- scale/__manifest__.py
+++ scale/__manifest__.py
@@ -1,39 +1,41 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Báscula",
+   'name': "Báscula",
 
-    'summary': """Báscula""",
+   'summary': """Báscula""",
 
-    'description': """
+   'description': """
         
     """,
 
-    'author': "Wissen",
-    'website': "http://www.yourcompany.com",
+   'author': "Wissen",
+   'website': "http://www.yourcompany.com",
 
+   'category': 'Báscula',
+   'version': '14.0.1',
 
-    'category': 'Báscula',
-    'version': '14.0.1',
+   'depends': ['sale_management', 'purchase', 'fleet', 'contacts',
+               'lob_extend'],
 
-    'depends': ['sale_management', 'purchase', 'fleet', 'contacts', 'lob_extend'],
-
-    # always loaded
-    'data': [
-        'security/groups.xml',
-        'security/ir.model.access.csv',
-        'report/scale_report.xml',
-        'report/scale_report_template.xml',
-        'views/scale_exit_views.xml',
-        'views/scale_entrance_views.xml',
-        'views/scale_driver_views.xml',
-        'views/scale_exit_orderline_views.xml',
-        #'wizard/bascule_exit_register_view.xml'
-    ],
-    # only loaded in demonstration mode
-    # 'demo': [
-    #    'demo.xml',
-    # ],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
+   # always loaded
+   'data': [
+      'security/groups.xml',
+      'security/ir.model.access.csv',
+      'report/scale_report.xml',
+      'report/scale_entrance_report_template.xml',
+      'report/scale_exit_report_template.xml',
+      'views/scale_exit_views.xml',
+      'views/scale_entrance_views.xml',
+      'views/scale_driver_views.xml',
+      'views/scale_exit_orderline_views.xml',
+      'report/scale_reporting_views.xml',
+      # 'wizard/bascule_exit_register_view.xml'
+   ],
+   # only loaded in demonstration mode
+   # 'demo': [
+   #    'demo.xml',
+   # ],
+   'installable': True,
+   'application': True,
+   'auto_install': False,
 }
--- scale/models/scale_entrance.py
+++ scale/models/scale_entrance.py
@@ -22,11 +22,12 @@ class ScaleEntrance(models.Model):
                             index=True, tracking=3,
                             default='draft')
 
-   type = fields.Char('Tipo', default='Entrada', required=True, readonly=True)
+   type = fields.Selection([('ent', 'Entrada')], 'Tipo', default='ent',
+                           required=True, readonly=True)
 
    plant_id = fields.Many2one('lob', 'Planta', default=None, required=True,
                               domain="[('scale_entrance','=',True)]",
-                              states=STATES, copy=False,
+                              states=STATES,
                               ondelete='restrict', tracking=True)
 
    @api.onchange('plant_id')
@@ -34,7 +35,7 @@ class ScaleEntrance(models.Model):
       self.order_id = None
       self.order_line_ids = None
 
-   order_id = fields.Many2one('purchase.order', 'Pedido de compra',
+   order_id = fields.Many2one('purchase.order', 'Número de orden de compra',
                               states=STATES, copy=False,
                               required=True, ondelete='cascade',
                               domain="[('state', '=', 'purchase'),('business_line','=',plant_id)]",
@@ -55,21 +56,20 @@ class ScaleEntrance(models.Model):
 
    rel_user = fields.Char('Comercial', related='order_id.user_id.name',
                           readonly=True)
-   rel_date_order = fields.Datetime('Fecha de pedido',
+   rel_date_order = fields.Datetime('Fecha de orden',
                                     related='order_id.date_order',
                                     readonly=True)
-   rel_customerId = fields.Integer('Cliente Id',
-                                   related='order_id.partner_id.id',
-                                   readonly=True)
-   rel_customer = fields.Char('Cliente',
-                              related='order_id.partner_id.name',
-                              readonly=True)
+   rel_idpartner = fields.Integer('Proveedor Id',
+                                  related='order_id.partner_id.id',
+                                  readonly=True)
+   rel_partner = fields.Char('Proveedor',
+                             related='order_id.partner_id.name',
+                             readonly=True)
 
-   unit_id = fields.Many2one('uom.uom', 'Unidad de medida',
+   unit_id = fields.Many2one('uom.uom', 'Unidad de báscula',
                              default=lambda x: x.env.ref(
                                 'uom.product_uom_kgm').id, ondelete='restrict',
-                             required=True,
-                             readonly=True)
+                             required=True, readonly=True)
 
    @api.onchange('order_id')
    def _onchangelines(self):
@@ -109,6 +109,7 @@ class ScaleEntrance(models.Model):
                                    readonly=True)
    exit_date = fields.Datetime('Hora y fecha de salida', readonly=True)
 
+
    @api.depends('order_line_ids', 'order_id')
    def _compute_lines(self):
       total = 0
@@ -126,6 +127,11 @@ class ScaleEntrance(models.Model):
          vals['name'] = seq.next_by_code(code) or 'Nuevo'
       result = super(ScaleEntrance, self).create(vals)
       return result
+
+   def unlink(self):
+      if self.state == 'sent':
+         raise ValidationError(_('No se puede eliminar báscula enviada, existen movimientos'))
+      return super(ScaleEntrance, self).unlink()
 
    def action_confirm(self):
       if not 'draft' in self.order_line_ids.mapped('state'):
