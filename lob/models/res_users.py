77f54103dae714a776dcca45ac59f881e8773c7d
3lnumag <3lnumag@gmail.com>
2021-02-16 23:12:28 -0600

    [FIX] lob groups

--- lob/models/res_users.py
+++ lob/models/res_users.py
@@ -7,11 +7,24 @@ from odoo.exceptions import ValidationError
 class ResUsers(models.Model):
    _inherit = 'res.users'
 
-   business_line_ids = fields.Many2many('lob', string='Líneas de negocio permitidas')
+   business_line_ids = fields.Many2many('lob',
+                                        string='Líneas de negocio permitidas')
    business_line_id = fields.Many2one('lob', 'Línea de negocio predeterminada')
-
 
    @api.constrains('business_line_id', 'business_line_ids')
    def _onchange_lob(self):
-      if not any(line.id == self.business_line_id.id for line in self.business_line_ids):
-         raise ValidationError("La línea de negocio predeterminada no se encuentra dentro de las permitidas")
+      group_lob = self.env.ref('lob.group_lob', False)
+      flag_lob = group_lob in self.groups_id
+      for user in self:
+         if user.business_line_id and user.business_line_ids:
+            if not (user.business_line_id in user.business_line_ids):
+               raise ValidationError(
+                  "La línea de negocio predeterminada no se encuentra dentro de las permitidas")
+            else:
+               if flag_lob:
+                  user.write({'groups_id': [(3, group_lob.id)]})
+                  user.write({'groups_id': [(4, group_lob.id)]})
+               else:
+                  user.write({'groups_id': [(4, group_lob.id)]})
+         else:
+            user.write({'groups_id': [(3, group_lob.id)]})
