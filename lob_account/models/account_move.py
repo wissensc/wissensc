336b0aba6dc64426538ef436d1e078fa7ed4e4ee
3lnumag <3lnumag@gmail.com>
2021-03-18 18:36:30 -0600

    [UPD] uso y metodo de cliente en facturas

--- lob_account/models/account_move.py
+++ lob_account/models/account_move.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class AccountMove(models.Model):
    _inherit = 'account.move'
 
    project_id = fields.Many2one('account.move.project', "Proyecto", ondelete="restrict")
+
+   @api.onchange('partner_id')
+   def _resetOrder(self):
+      self.l10n_mx_edi_payment_method_id = self.partner_id.payment_method_id.id
+      self.l10n_mx_edi_usage = self.partner_id.edi_usage
