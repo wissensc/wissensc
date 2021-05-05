1622a4dba085edf1b58e758a6c97a37b1ea0c506
3lnumag <3lnumag@gmail.com>
2021-05-05 11:17:48 -0500

    [UPD] Otra vez Matrix

--- scale/models/entrance_orderline.py
+++ scale/models/entrance_orderline.py
@@ -64,9 +64,9 @@ class ScaleOrderLine(models.Model):
                  'x-api-key': api_key,
                  }
       lob = {
-         'Planta Teotihuacán': 'Teotihuacan',
-         'Planta Xalostoc': 'Teotihuacan',
-         'Oficinas Xalostoc': 'Teotihuacan'
+         'Planta Teotihuacán': 'Matrix',
+         'Planta Xalostoc': 'Matrix',
+         'Oficinas Xalostoc': 'Matrix'
       }
       type = {'entrance': 'UNLOAD', 'exit': 'LOAD'}
 
